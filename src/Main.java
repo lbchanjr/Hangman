8d185b3901b7e799af8093820750c690e28d803d
Louise Chan <lbchan525@gmail.com>
2020-01-26 23:55:08 -0500

    Removed unused import declarations

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
